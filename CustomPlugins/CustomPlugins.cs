4f130a57953b18e9e856b294bd0d1f8fd622280d
Konstantinos Danochristos <konstantinos.danochristos@gmail.com>
2020-10-06 15:35:04 +0300

    first commit

---
+++ CustomPlugins/CustomPlugins.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using Microsoft.Xrm.Sdk;
+
+namespace CustomPlugins
+{
+    public class CustomPlugins : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+			var context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+			var entity = context.InputParameters["Target"] as Entity;
+
+			if (entity.Attributes.ContainsKey("orfium_custommusictype"))
+			{
+				OptionSetValueCollection genres = new OptionSetValueCollection();
+				foreach (var option in entity.GetAttributeValue<EntityReference>("orfium_custommusictype").ToString().Split(','))
+				{
+					genres.Add(new OptionSetValue(int.Parse(option)));
+				}
+
+				entity["orfium_typeofmusic"] = genres;
+			}
+		}
+    }
+}
